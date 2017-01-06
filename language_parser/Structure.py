6f4a9d72760e4b2e661e79e67d028dae74dc73b8
arash <arash.zamani@bihe.org>
2017-01-06 10:27:02 +0330

    version 1.2.2

--- language_parser/Structure.py
+++ language_parser/Structure.py
@@ -1,10 +1,15 @@
 from __future__ import unicode_literals
 from hazm import *
 import language_parser.Sentence as Sentence
+import language_parser.word as w
+import language_parser.Word as wordC
+from gensim import corpora
 
 class Structure:
     text = ""
     sentences = list()
+    sentences_obj = list()
+    words_in_sentences = list()
     tags = dict()
     chunks = dict()
 
@@ -12,11 +17,19 @@ class Structure:
         self.text = text
         self.prepare_sentences()
 
+    # def __iter__(self):
+    #     for sentence in self.sentences:
+    #         yield sentence.sentence_to_word(sentence)
+
     def prepare_sentences(self):
-        temp_sentences = Sentence.sent_tokenize(self.text)
-        for temp in temp_sentences:
-            sent = Sentence.Sentence(temp)
-            self.sentences.append(sent)
+        self.sentences = Sentence.Sentence.parse_sentences(self.text)
+        self.sentences_obj = [Sentence.Sentence(sentence) for sentence in self.sentences]
+
+    def prepare_list_of_words_in_sentences(self):
+        if len(self.words_in_sentences) == 0:
+            stop_list = wordC.Word.get__stop_words()
+            self.words_in_sentences = [[word for word in word_tokenize(sentence) if word not in w.specials] for sentence in self.sentences]
+        return self.words_in_sentences
 
     def generate_tags_dict(self):
         for temp in self.sentences:
@@ -30,11 +43,11 @@ class Structure:
                     self.tags[part[1]] = values
 
     def generate_chunks_dict(self):
-        for temp in self.sentences:
-            temp_chunks = temp.parse_chunks()
-            for key in temp_chunks.keys():
-                if key in self.chunks.keys():
-                    self.chunks[key].append(temp_chunks[key])
-                else:
-                    self.chunks[key] = temp_chunks[key]
-
+        self.chunks = corpora.Dictionary(sentence.parse_chunks() for sentence in self.sentences_obj)
+        # for temp in self.sentences:
+        #     temp_chunks = temp.parse_chunks()
+        #     for key in temp_chunks.keys():
+        #         if key in self.chunks.keys():
+        #             self.chunks[key].append(temp_chunks[key])
+        #         else:
+        #             self.chunks[key] = temp_chunks[key]
