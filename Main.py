11c6e57d7e5f6ee7d0b83ae33359b10b9b788cad
Fares Hedayati <fares.hedayati@gmail.com>
2017-03-04 18:30:17 +0000

    added word_embedding_al1

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ import test_cases.structure_generation as sg
 import test_cases.algorithm1 as al1
 import test_cases.algorithm2 as al2
 import test_cases.algorithm3 as al3
-
+import test_cases.word_embedding_al1 as we 
 
 def main():
     print('Started...')
@@ -19,7 +19,8 @@ def main():
     # model = sg.StructureModel(myfile)
     # model.model()
 
-    al = al3.StructureModel(myfile)
+    #al = al3.StructureModel(myfile)
+    al = we.StructureModel(myfile)
     al.model()
     # struct = structure.Structure(myfile.text)
     # # struct.generate_chunks_dict()
